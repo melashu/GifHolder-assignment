4cba67faa2daf2c6fb570abddb4a19974d9fded1
melashu <meshu102@outlook.com>
2023-03-09 14:39:17 +0300

    Remove head

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::API
   before_action :authenticate_user
 
   def authenticate_user
-    current_user || head(401)
+    current_user
   end
 
   def current_user
     token = params[:access_token] || request.headers['Authorization']
+    # p token
     if token
       id = JWT.decode(token, Rails.application.credentials[:secret_key_base], true, { algorithm: 'HS256' })[0]['user_id']
       @current_user ||= User.find(id)
