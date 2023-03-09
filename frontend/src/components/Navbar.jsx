c6cb178602aff6e564d34b183045b27e14b90ea4
melashu <meshu102@outlook.com>
2023-03-09 14:40:49 +0300

    Add gif and saved gif link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import Button from './common/Button';
+import { Link } from 'react-router-dom';
 
 const Nav = styled.nav`
   height: 64px;
@@ -10,20 +11,23 @@ const Nav = styled.nav`
   width: 100%;
   top: 0;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
   background: white;
   box-shadow: 0 1px 4px rgba(0,0,0,.1);
   padding: 0 16px;
+  gap:20px;
   box-sizing: border-box;
 `;
+
 
 export default function Navbar() {
   const auth = useContext(AuthContext)
 
   return (
     <Nav>
-      <div></div>
+      <Link to="/">Home</Link>
+      <Link to="/saved">Saved Gif</Link>
       <Button onClick={auth.logout}>Logout</Button>
     </Nav>
   )
